510f580bf03c2f0bb410bb02f9430f25055823a7
Shirley Xu <shirley.t.xu@gmail.com>
2021-09-25 21:11:58 -0700

    Add chapter 4 (cropping / manipulating pixels) and more specific comments

--- opencvproject.py
+++ opencvproject.py
@@ -3,6 +3,7 @@ import argparse
 import imutils
 import cv2
 
+# chapter 3: loading images
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required = True, help = "Path to the image")
 args = vars(ap.parse_args())
@@ -11,6 +12,7 @@ image = cv2.imread(args["image"])
 cv2.imshow("Original", image)
 cv2.waitKey(0)
 
+# chapter 6: image transformation (rotation and scaling)
 # image is rotated around point representing 1/3 of width and 1/3 of height
 (h, w) = image.shape[:2]
 pointMoveAround = (w//3, h//3)
@@ -26,7 +28,15 @@ rotated = imutils.rotate(image, 30)
 cv2.imshow("Rotated by 30 degrees", rotated)
 cv2.waitKey(0)
 
+# chapter 8: blurring
 # gaussian blur with kernel size 9x9
 blurred = cv2.GaussianBlur(image, (9, 9), 0)
 cv2.imshow("Gaussian", blurred)
 cv2.waitKey(0)
+
+# chapter 4: accessing and manipulating pixels
+# grabs 400 x 800 pixel region from upper left of image
+corner = image[0:450, 0:800]
+cv2.imshow("Corner", corner)
+cv2.waitKey(0)
+
